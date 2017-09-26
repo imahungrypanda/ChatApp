07648b7df24bdd7c83d5732d4f892b3854840774
Steve Olsen <stevelolsen@gmail.com>
2017-09-26 14:59:13 -0700

    Clean up

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,3 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = require('socket.io-client')();
-  // const io = socket.connect('http://localhost:8000');
-  // console.log(io);
-
 });
