569e2afa5e6424e288a57fca1381c638f06f36b5
Steve Olsen <stevelolsen@gmail.com>
2017-09-26 16:42:06 -0700

    Chat can send messages

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const socket = require('socket.io-client')();
+  const ChatUI = require('./chatUI');
+  const myChat = new ChatUI(socket);
+
+  socket.on('message', (message) => {
+    myChat.addMessage(message.text);
+  })
 });
